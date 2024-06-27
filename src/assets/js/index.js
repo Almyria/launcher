7cb53da5c8dac7c1cb5ad6e7d2e860b6526859c8
Le_Mocha <cadet@manoah.fr>
2024-06-28 00:59:35 +0200

    Oopsie OpenDevTools Kappa

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -123,8 +123,8 @@ function sleep(ms) {
 }
 
 document.addEventListener("keydown", (e) => {
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
+    /* if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
         ipcRenderer.send("update-window-dev-tools");
-    }
+    } */
 })
 new Splash();
